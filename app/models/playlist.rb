a1ca80334c71fc40a9f655d04c3682d1a5d3c16c
Grycek <adam.grycner@wp.pl>
2010-10-22 18:41:38 +0200

    Callback (adding intro song after playlist creation)

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,5 @@
 class Playlist < ActiveRecord::Base
   has_many :songs, :dependent => :destroy
   validates_presence_of :description
+  after_create {|playlist| playlist.songs.create(:artist => 'Automatycznie dodawany utwór', :title => 'Intro') }
 end
